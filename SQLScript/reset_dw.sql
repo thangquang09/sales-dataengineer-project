89bb8761621dc52930bc48bc0e1cf7aea86f22c3
thangquang09 <lythang09092004@gmail.com>
2024-10-05 20:42:54 +0700

    update create view

--- SQLScript/reset_dw.sql
+++ SQLScript/reset_dw.sql
@@ -16,6 +16,9 @@ DROP TABLE Dim_Employee;
 DROP TABLE Dim_Store;
 DROP TABLE Dim_City;
 
+-- Make sure you created sales_dw database and use it to run this script
+-- RDBMS: PostgreSQL
+
 -- create Dim of sales first
 
 CREATE TABLE Dim_City (
@@ -148,3 +151,101 @@ CREATE TABLE Fact_Production (
 
 
 INSERT INTO Dim_Source_Online(source_online_id, link_name) VALUES (1000, 'offline');
+
+-- CREATE VIEW 
+
+-- Revenue, profit, number of orders, average value based on criteria: product, store, employee, city, customer
+-- Quantity of products sold by category, store, city
+-- Ratio of online/offline revenue and order numbers over time
+-- For online orders, the structure of order numbers and revenue by source ("Tiki", "Shopee", etc.)
+-- Total revenue by store, employee, product
+-- Top 10 best-selling and worst-selling products
+-- Top 10 employees and stores with the highest and lowest revenue.
+
+CREATE VIEW view_sales_summary AS
+SELECT
+	p.product_id,
+	p.name as product_name,
+	s.store_id,
+	s.name as store_name,
+	e.employee_id,
+	e.name as employee_name,
+	city.city_id,
+	city.name as city_name,
+	c.customer_id,
+	c.name as customer_name,
+	SUM(fo.revenue) AS revenue,
+	SUM(fo.revenue - fo.standardcost) as profit,
+	SUM(fo.number_order) as number_order,
+	AVG(fo.revenue) as average_value
+FROM
+	fact_sales_order fo
+	JOIN fact_production fp ON fo.date_id = fp.date_id
+	JOIN dim_product p ON fp.product_id = p.product_id
+	JOIN dim_store s ON fo.store_id = s.store_id
+	JOIN dim_city city ON s.city_id = city.city_id
+	JOIN dim_employee e ON fo.employee_id = e.employee_id
+	JOIN dim_customer c ON fo.customer_id = c.customer_id
+GROUP BY
+	p.product_id,
+	p.name,
+	s.store_id,
+	s.name,
+	e.employee_id,
+	e.name,
+	city.city_id,
+	city.name,
+	c.customer_id,
+	c.name;
+
+CREATE VIEW view_quantity_cate_store_city AS
+SELECT
+	cate.category_id,
+	cate.name as category_name,
+	s.store_id,
+	s.name as store_name,
+	city.city_id,
+	city.name as city_name,
+	SUM(fp.quantity) as quantity
+FROM
+	fact_production fp
+	JOIN dim_product p ON fp.product_id = p.product_id
+	JOIN dim_category cate ON cate.category_id = p.category_id
+	JOIN dim_store s ON s.store_id = fp.store_id
+	JOIN dim_city city ON s.city_id = city.city_id
+GROUP BY
+	cate.category_id,
+	cate.name,
+	s.store_id,
+	s.name,
+	city.city_id,
+	city.name;
+
+CREATE VIEW view_online_offline_summary AS 
+SELECT
+	d.date_id,
+	m.month_id,
+	m.name as month_name,
+	y.year_id,
+	y.name as year,
+	fo.source_online_id,
+	so.link_name,
+	SUM(fo.number_order_online) as number_order_online,
+	SUM(fo.number_order_offline) as number_order_offline,
+	SUM(fo.revenue) as revenue,
+	SUM(fo.revenue_online) as revenue_online,
+	SUM(fo.revenue_offline) as revenue_offline
+FROM
+	fact_sales_order fo
+	JOIN dim_date d ON d.date_id = fo.date_id
+	JOIN dim_month m ON m.month_id = d.month_id
+	JOIN dim_year y ON y.year_id = d.year_id
+	JOIN dim_source_online so ON so.source_online_id = fo.source_online_id
+GROUP BY
+	d.date_id,
+	m.month_id,
+	m.name,
+	y.year_id,
+	y.name,
+	fo.source_online_id,
+	so.link_name;
