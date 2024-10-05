8f7ef07fa2dc96dc234e6d59e575ec69133605a5
thangquang09 <lythang09092004@gmail.com>
2024-10-05 17:03:07 +0700

    add load_fact_sales sql script

--- load_fact_sales.sql
+++ load_fact_sales.sql
@@ -1,22 +1,13 @@
 SELECT 
-    order_id,
-    orderdate,
-    source_online_id,
-    customer_id,
-    employee_id,
-    store_id,
-    total as revenue,
-    SUM(CASE WHEN isOnline = TRUE THEN total ELSE 0 END) AS revenue_online,
-    SUM(CASE WHEN isOnline = FALSE THEN total ELSE 0 END) AS revenue_offline,
-	COUNT(*) AS number_order,
-	COUNT(CASE WHEN isOnline = TRUE THEN 1 ELSE 0 END) AS number_order_online,
-	COUNT(CASE WHEN isOnline = FALSE THEN 1 ELSE 0 END) AS number_order_offline,
-	SUM(standardCost) AS standardCost,
-	SUM(total) - SUM(standardCost) AS profit
-	
-FROM
+    store_id, employee_id, customer_id, orderdate, source_online_id,
+    SUM(total) AS revenue,
+	SUM(standardcost) AS standardcost,
+    SUM(total - standardCost) AS profit,
+    COUNT(order_id) AS number_order,
+	SUM(CASE WHEN isOnline = true THEN total ELSE 0 END) AS revenue_online,
+	SUM(CASE WHEN isOnline = false THEN total ELSE 0 END) AS revenue_offline,
+    SUM(CASE WHEN isOnline = true THEN 1 ELSE 0 END) AS number_order_online,
+    SUM(CASE WHEN isOnline = false THEN 0 ELSE 1 END) AS number_order_offline
+FROM 
     sales.order
-WHERE
-    isProcessed = FALSE
-GROUP BY
-	order_id, orderdate, source_online_id, customer_id, employee_id, store_id, total
+GROUP BY store_id, employee_id, customer_id, orderdate, source_online_id;
