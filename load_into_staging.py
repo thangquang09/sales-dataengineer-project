0ea291895329fadc67803f46f94c3090d0b4a2bc
thangquang09 <lythang09092004@gmail.com>
2024-10-08 13:36:21 +0700

    update script, code more clean

--- load_into_staging.py
+++ load_into_staging.py
@@ -25,39 +25,30 @@ EDR = {
 
 
 
-def load_table_to_staging(schema, table, mysql_engine, mysql_session, postgres_engine, postgress_session, batch_size=10000):
-    mysql_columns = [col[0].lower() for col in mysql_session.execute(text(f'SHOW COLUMNS FROM {schema}_{table}')).fetchall()]
-    mysql_columns.remove('checkstatus')
-    mysql_data = mysql_session.execute(text(f'SELECT * FROM {schema}_{table} WHERE checkStatus != 1')).fetchall()
+def load_table_to_staging(schema, table, mysql_engine, mysql_session, postgres_engine, postgress_session, batch_size=1):
+    query = text(f'SELECT * FROM {schema}_{table} WHERE checkStatus != 1')
+    df = pd.read_sql(query, mysql_engine)
 
-    if not mysql_data:
-        print(f'{schema}_{table} is up to date')
-        return
+    df.columns = [col.lower() for col in df.columns]
+    df.drop(columns=['checkstatus'], inplace=True) # remove checkStatus column
 
     new_columns = ['insertdate', 'updatedate', 'sourcesystem', 'isprocessed']
     default_values = [datetime.now().strftime('%Y-%m-%d'), None, 'MySQL', False]
-    mysql_columns.extend(new_columns)
 
-    data_to_upsert = []
-    for row in mysql_data:
-        row_with_default = list(row[:-1]) + default_values # remove checkStatus
-        full_data = dict(zip(mysql_columns, row_with_default))  
-        data_to_upsert.append(full_data)
+    for col, val in zip(new_columns, default_values):
+        df[col] = val
 
-        # call upsert_data when data_to_upsert has enough
-        if len(data_to_upsert) >= batch_size:
-            upsert_data(postgres_engine, postgress_session, table, schema, data_to_upsert, mysql_columns[0])
-            data_to_upsert = []  # Reset list to upsert
-    # insert the remaining data
-    if data_to_upsert:
-        upsert_data(postgres_engine, postgress_session, table, schema, data_to_upsert, mysql_columns[0])
-    print(f'Upserted {len(mysql_data)} records from {schema}_{table} to {schema}_{table} in PostgreSQL')
-    update_status_mysql = text(f'UPDATE {schema}_{table} SET checkStatus = 1 WHERE checkStatus != 1')
-    mysql_session.execute(update_status_mysql)
+    if 'source_online_id' in df.columns:
+        df.fillna({'source_online_id': 1000}, inplace=True) # 1000 is offline
+
+    load_with_batch(postgres_engine, postgress_session, table, schema, df, batch_size)
+    update_stmt = text(f'UPDATE {schema}_{table} SET checkStatus = 1 WHERE checkStatus != 1')
+    mysql_session.execute(update_stmt)
     mysql_session.commit()
-    print(f'Updated checkStatus for {schema}_{table} in MySQL')
 
-def load_to_staging(EDR, mysql_engine, mysql_session, postgres_engine, postgress_session, batch_size=10000):
+    
+
+def load_to_staging(EDR, mysql_engine, mysql_session, postgres_engine, postgress_session, batch_size=1000):
     print('Start loading data to staging...')
     print('Batch size:', batch_size)
     print('----------------------------------')
