a2f42d9542ea5978afd54b0934240f20fea796f3
thangquang09 <lythang09092004@gmail.com>
2024-10-22 11:37:56 +0700

    updae compose: install python module for airflow only 1 time

--- script/entrypoint.sh
+++ script/entrypoint.sh
@@ -2,10 +2,19 @@
 
 set -e
 
+# Kiểm tra nếu thư viện đã được cài đặt
 if [ -e "/opt/airflow/requirements.txt" ]; then
-    $(command -v pip) install --user -r requirements.txt
+    if [ ! -f "/usr/local/lib/python3.10/site-packages/.requirements_installed" ]; then
+        echo "Installing Python dependencies from requirements.txt"
+        $(command -v pip) install --user -r /opt/airflow/requirements.txt
+        # Tạo file flag để ghi nhớ việc đã cài đặt thư viện
+        touch /usr/local/lib/python3.10/site-packages/.requirements_installed
+    else
+        echo "Python dependencies are already installed."
+    fi
 fi
 
+# Kiểm tra và khởi tạo cơ sở dữ liệu Airflow nếu chưa có
 if [ ! -f "/opt/airflow/airflow.db" ]; then
     airflow db init && \
     airflow users create \
@@ -17,6 +26,8 @@ if [ ! -f "/opt/airflow/airflow.db" ]; then
         --password admin
 fi
 
+# Nâng cấp cơ sở dữ liệu Airflow
 $(command -v airflow) db upgrade
 
+# Khởi động webserver
 exec airflow webserver
